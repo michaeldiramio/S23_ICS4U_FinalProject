ee1e31893380be4080fc407214bf13a367fc3220
julianmashedpotatoes <98428340+julianmashedpotatoes@users.noreply.github.com>
2023-05-30 00:17:09 +0000

    Made Control to allow other classes to access a player's current pressed keys, and added run() and DInit() methods to main

--- Main.java
+++ Main.java
@@ -3,12 +3,36 @@ import java.awt.Color;
 
 public class Main {
 
-	public static void main(String[] args) {
-		DConsole dc = new DConsole();
-		dc.setPaint(Color.RED);
-		dc.fillRect(200, 50, 80, 20);
-		dc.redraw();
-	}
+  // data
+  private DConsole dc;
+
+  // initializes DConsole
+  public void DInit(int width, int height) {
+    this.dc = new DConsole(width, height);
+    this.dc.setOrigin(this.dc.ORIGIN_CENTER);
+  }
+
+  // runs methods from other classes
+  public void run() {
+    this.dc.clear();
+
+    this.dc.setPaint(Color.RED);
+    this.dc.fillEllipse(300, 300, 60, 40);
+
+    this.dc.redraw();
+    this.dc.pause(100);
+  }
+
+  // main
+  public static void main(String[] args) {
+
+    Main m = new Main();
+    
+    m.DInit(600, 600);
+    while(true) {
+      m.run();
+    }
+  }
 	
 	
 }

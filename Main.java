cda3ace4687124306cc6823d34a73a791d7ad305
julianmashedpotatoes <98428340+julianmashedpotatoes@users.noreply.github.com>
2023-06-01 23:59:01 +0000

    Finished creating WordInput, fully commented Control and WordInput

--- Main.java
+++ Main.java
@@ -6,53 +6,23 @@ public class Main {
 
   // data
   private DConsole dc;
+  WordInput wordInput;
+  private String finalWord = "";
+  
 
   // initializes DConsole
   public void DInit(int width, int height) {
     this.dc = new DConsole(width, height);
     this.dc.setOrigin(this.dc.ORIGIN_CENTER);
+    this.wordInput = new WordInput(10, this.dc);
   }
 
   // runs methods from other classes
   public void run() {
+    this.dc.clear();
     
-
-    ArrayList<Player> playerList = new ArrayList<>();
-    playerList.add(new Player(1,Color.RED,300,300,this.dc));
-    playerList.add(new Player(2,Color.BLUE,200,200,this.dc));
-    playerList.add(new Player(3,Color.GREEN,400,400,this.dc));
-    playerList.add(new Player(4,Color.BLACK,500,500,this.dc));
-
-    ArrayList<Control> controlList = new ArrayList<>();
-    controlList.add(new Control(0, this.dc));
-    controlList.add(new Control(1, this.dc));
-    controlList.add(new Control(2, this.dc));
-    controlList.add(new Control(3, this.dc));
-
-    while(true) {
-      this.dc.clear();
-      for(int i = 0; i < playerList.size(); i++) {
-      boolean[] tempControl = controlList.get(i).getPlayerKeysPressed();
-      if(tempControl[0] == true) {
-        playerList.get(i).move(0, 5);
-      }
-      System.out.println(tempControl[i]);
-      playerList.get(i).draw();
-    }
-      this.dc.redraw();
+    this.dc.redraw();
     this.dc.pause(100);
-    }
-    
-
-
-    
-
-    
-  }
-
-  public void firstCycle() {
-    
-    
   }
 
   // main
@@ -60,7 +30,6 @@ public class Main {
 
     Main m = new Main();
     m.DInit(600, 600);
-    m.firstCycle();
     while(true) {
       m.run();
     }
--- WordInput.java
+++ WordInput.java
@@ -4,78 +4,178 @@ import java.awt.*;
 import java.util.*;
 
 public class WordInput {
+
+  // constants
+  private Integer[] tempIllegalKeycodes = {0,9,20,16,10,8,16,38,37,40,39,17,18,91};
+  private ArrayList<Integer> illegalKeycodes = new ArrayList<>();
   
   // data
   private DConsole dc;
   private String word;
+  private int characterLimit;
   private ArrayList<Character> keyInput;
 
   
-  // constructor
+  // constructor with preset characterLimit
   public WordInput(DConsole dc) {
     this.dc = dc;
-    this.word = null;
-    this.keyInput = null;
+    this.word = "";
+    this.characterLimit = 99;
+    this.keyInput = new ArrayList<Character>(1);
+
+    // fills illegalKeycodes (ArrayList) with tempIllegalKeycodes values
+    for(int i = 0; i < this.tempIllegalKeycodes.length; i++) {
+      this.illegalKeycodes.add(tempIllegalKeycodes[i]);
+    }
+  }
+
+  // constructor with dynamic characterLimit
+  public WordInput(int characterLimit, DConsole dc) {
+    this.dc = dc;
+    this.word = "";
+    this.characterLimit = characterLimit;
+    this.keyInput = new ArrayList<Character>(1);
+
+    // fills illegalKeycodes (ArrayList) with tempIllegalKeycodes values
+    for(int i = 0; i < this.tempIllegalKeycodes.length; i++) {
+      this.illegalKeycodes.add(tempIllegalKeycodes[i]);
+    }
   }
 
   
   /*
   // METHODS
   */
-  
-  // gets current keycodes pressed
+
+  // gets current keys pressed
   private int[] getCurrentKeycodesPressed() {
-    int[] tempKeycodes = this.dc.getKeys();
+    // gets current keys pressed from the DConsole (may be null if no keys are pressed)
+    int[] tempKeycodes = this.dc.getKeys(); 
+    
     if(tempKeycodes.length > 0) {
       Arrays.sort(tempKeycodes);
-      return tempKeycodes;
+      return tempKeycodes; // returns an integer array of currently pressed sorted keycodes
     } else {
-      return null;
+      return new int[1]; // if no keys are pressed the method will return an int array of 1 zero instead of null
     }
   }
 
   
-  // gets current keys pressed (as a char)
-  private char[] getPlayerKeysPressed() {
+  // gets current keys pressed (as a char array)
+  private char[] getCurrentKeysPressed() {
+    int[] tempKeycodes = this.getCurrentKeycodesPressed(); // gets the current keycodes (int array) 
+                                                  // from previous method
+    char[] tempKeys = new char[tempKeycodes.length];
+
+    // converts keycodes (int) to its lowercase char value. does not convert a value if it is an illegal keypress
+    for(int i = 0; i < tempKeycodes.length; i++) {
+      if(!illegalKeycodes.contains(tempKeycodes[i])) {
+        tempKeys[i] = Character.toLowerCase((char)tempKeycodes[i]);
+      }
+    }
+    return tempKeys;
+  }
+
+
+  // gets the current keys and keypresses and adds 
+  public void refreshKeys() {
+    
+    // gets both current keycodes (int) and keys (char)
+    char[] tempKeys = this.getCurrentKeysPressed();
     int[] tempKeycodes = this.getCurrentKeycodesPressed();
 
-    if(tempKeycodes != null) {
-      char[] tempKeys = new char[tempKeycodes.length];
-      for(int i = 0; i < tempKeycodes.length; i++) {
-        tempKeys[i] = (char)tempKeycodes[i];
+    // if the keycode for shift exists in the array set shiftPressed to true
+    boolean shiftPressed = (Arrays.binarySearch(tempKeycodes, 16) > -1) ? true: false;
+
+    // if the keycode for backspace exists in the array and the current word legth is greater than zero, 
+    // delete the last letter on word
+    if((Arrays.binarySearch(tempKeycodes, 8) > -1) && this.word.length() > 0) {
+      this.word = this.word.substring(0, this.word.length() - 1);
+    }
+
+    for(int i = 0; i < tempKeys.length; i++) {
+      // checks that current keys are not the same as past keys, checks that word is below character limit, 
+      // and checks that the keys are not '\0' (undefined char)
+      if(!this.keyInput.contains(tempKeys[i]) && this.word.length() < this.characterLimit && tempKeys[i] != '\0') {
+        // if shift is pressed then convert each char being added to word to uppercase, otherwise just add chars to word
+        this.word += (shiftPressed) ? Character.toUpperCase(tempKeys[i]) : tempKeys[i];
       }
-      return tempKeys;
+    }
+
+    // clears last keys and sets it to current keys
+    this.keyInput.clear();
+    for(int i = 0; i < tempKeys.length; i++) {
+      this.keyInput.add(tempKeys[i]);
+    }
+  }
+
+
+  // gets if a key is pressed or not, char key must be lowercase
+  public boolean keyPressed(char key) {
+    int keycode = key; // converts key (char) into its keycode (int)
+    int[] tempKeycodes = this.getCurrentKeycodesPressed(); // gets current keycodes (int array)
+
+    // returns true if desired keycode (formerly key) is in the current keycode array
+    if(Arrays.binarySearch(tempKeycodes, keycode) > -1) {
+      return true;
     } else {
-      return null;
+      return false;
     }
   }
 
-  public void refreshKeys() {
-    char[] tempKeys = this.getPlayerKeysPressed();
+  
+  // gets current word (string stored in WordInput object)
+  public String getCurrentWord() {
+    return this.word;
+  }
+
+  
+  // gets access to the final word once enter is pressed. otherwise it returns ""
+  public String getFinalWord() {
+    int[] tempKeycodes = this.getCurrentKeycodesPressed(); // get current keycodes (int array)
+    String tempWord = this.word; // stores the current word so it can be returned after word is set back to ""
+
+    // if the enter keycode is in the tempKeycodes array, return the current word
+    if(Arrays.binarySearch(tempKeycodes, 10) > -1) {
+      this.word = "";
+      return tempWord;
+    } else {
+      return ""; // if enter is not pressed, return a blank string (not null)
+    }
+  }
+
+
+  /*    EXAMPLE OF USING WORDINPUT
+
+  ***  after you use getFinalWord() it will be set to "" (a blank string, not null) ***
+
+
+  String finalWord = "";
+
+  while(true) {
+    wordInput.refreshKeys();
+    dc.drawString(wordInput.getCurrentWord(), 220, 150);
     
-    if(this.keyInput != null) {
-      for(int i = 0; i < keyInput.size(); i++) {
-        if(!this.keyInput.contains(tempKeys[i])) {
-          this.word += tempKeys[i];
-        }
-      }
+    String tempWord = wordInput.getFinalWord();
 
-      this.keyInput.clear();
-      for(int i = 0; i < tempKeys.length; i++) {
-        this.keyInput.add(tempKeys[i]);
-      }
+    if(tempWord != "") {
+      finalWord = tempWord;
     }
+    
+    dc.drawString(finalWord, 400, 150);
   }
 
-  public void printKeyInput() {
-    if(this.keyInput != null) {
-        for(int i = 0; i < this.keyInput.size(); i++) {
-        System.out.print(this.keyInput.get(i));
-      }
-      System.out.println();
-    }
-  }
 
-  
-  
+*** EXPLAINATION ***
+
+finalWord must be initialized, as DConsole cannot print a null string, it must be at least set to ""
+
+For any use of WordInput, make sure you use refreshKeys() before any code to get the most recent key input
+
+getCurrentWord() exists as a way to display the current typed word on the screen
+
+getFinalWord() will return "" until the enter key is pressed, whereupon it will return the current word.
+After getFinalWord() is called, word will be set back to ""
+
+  */
 }

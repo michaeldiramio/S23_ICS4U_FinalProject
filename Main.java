355c88703f731e5a75fbc71164caa349d9a5eb5f
abeerahmed2464 <133286084+abeerahmed2464@users.noreply.github.com>
2023-06-02 23:30:14 +0000

    implemented kieran's scene class into main, currently unfinished

--- Main.java
+++ Main.java
@@ -15,15 +15,20 @@ public class Main {
 
   // runs methods from other classes
   public void run() {
-    //Players
     ArrayList<Player> playerList = new ArrayList<>();
-    playerList.add(new Player(1,Color.RED,300,300,this.dc));
-    playerList.add(new Player(2,Color.BLUE,200,200,this.dc));
-    playerList.add(new Player(3,Color.GREEN,400,400,this.dc));
-    playerList.add(new Player(4,Color.BLACK,500,500,this.dc));
 
     //-----------------------Testing Chamber---------------------
     Minigame test = new TestGame(0, dc, playerList);
+
+    //Game screen testing (currently unfinished)
+    GameScreen n = new GameScreen(dc);
+    n.join(); //go through the joining process (code doesnt get passed here, waiting for kieran's input for this)
+    boolean[] playersPresent = n.playerInit(); //get an array of which players are playing
+    for (int i = 0; i < playersPresent.length; i++) { //create the players 
+      if (playersPresent[i]) { //player at i is playing
+        playerList.add(new Player(i + 1, Color.BLACK, 200, 200, dc)); //make that player 
+      }
+    }
 
     test.play();
     //---------------------------------------------------------
@@ -33,7 +38,7 @@ public class Main {
   // main
   public static void main(String[] args) {
     Main m = new Main();
-    m.DInit(600, 600); 
+    m.DInit(800, 550); 
     while(true) {
       m.run();
     }

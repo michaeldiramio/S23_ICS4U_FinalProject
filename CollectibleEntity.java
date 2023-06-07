ddfb0863824a6b5ebdbf7759c6b102b55f00bef4
Andrew-p1 <133383654+Andrew-p1@users.noreply.github.com>
2023-06-07 17:17:48 +0000

    Stopgate Commit 06/07

---
+++ CollectibleEntity.java
@@ -0,0 +1,13 @@
+import DLibX.*;
+import java.awt.Color;
+
+public class CollectibleEntity extends Entity{
+
+  public CollectibleEntity(DConsole dc, double x,double  y, double h, double w, Color color ){
+    super(dc, x, y, h, w, color);
+  }
+
+
+
+  
+}

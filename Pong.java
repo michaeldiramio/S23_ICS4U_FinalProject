33352aa28b378c9e7738e2944560860257a66d66
Knighty2 <105297764+Knighty2@users.noreply.github.com>
2023-06-13 18:07:02 +0000

    pong

--- Pong.java
+++ Pong.java
@@ -5,8 +5,8 @@ import java.util.*;
 public class Pong extends Minigame {
 
   //Constructor
-  public Pong (int id, DConsole dc, ArrayList<Player> playerList, ArrayList<Entity> entityList) {
-    super(1, dc, playerList, entityList);
+  public Pong (int id, DConsole dc, ArrayList<Player> playerList) {
+    super(1, dc, playerList);
   }
   
   //Play the game
@@ -17,61 +17,52 @@ public class Pong extends Minigame {
 
     ArrayList<Player> tempPlayerList = new ArrayList<>();
     for (int i = 0; i < playerList.size(); i++) {
-      tempPlayerList.set(i, playerList.get(i));
+      Player temp = playerList.get(i);
+      tempPlayerList.add(temp);
     }
 
-    int x[] = new int[2];
-    int y[] = new int[2];
-
-    x[0] = 50;
-    x[1] = 750;
-    y[0] = 275;
-    y[1] = 275;
-
-    int speed = 2;
-
+    int ballSpeed = 4;
+    
     //ball
     int bx = 400;
     int by = 275;
-    int bxc = 5;
-    int byc = 5;
+    int bxc = ballSpeed;
+    int byc = ballSpeed;
     Random rand = new Random();
     if(rand.nextInt(2) == 1) { //random start direction
       bxc*=-1;
     }
 
+    playerList.get(0).setSize(20, 100);
+    playerList.get(1).setSize(20, 100);
+
+    playerList.get(0).setX(50);
+    playerList.get(1).setX(750);
+    playerList.get(0).setY(275);
+    playerList.get(1).setY(275);
+
     while (game) { //these will be the loops that go on until game ends (refer to useful information for time limits)
       dc.clear();
-      in.refreshKeys();
 
-      // goes through each player to confirm if they are joining the game based on their keypresses
-      for(int i = 0; i < tempPlayerList.size()-2; i++) {
-        boolean[] tempControl = playerList.get(i).getControl().getPlayerKeysPressed();
-        if(tempControl[0]) { //up
-          if (y[i] > 20) {
-            y[i]-=speed;
-          }
-        } else if(tempControl[2]) { //down
-          if (y[i] < 530) {
-            y[i]+=speed;
-          }
-        }
-      }
-      
-      dc.fillRect(x[0], y[0], 20, 100); //p1
-      dc.fillRect(x[1], y[1], 20, 100); //p2
+      moveCharacters();
+      super.refreshScreen();
 
       //collision of ball with up/down edges
       if(by <= 10 || by >= 540) { 
-          byc*=-1;
-        }
+        byc*=-1;
+      }
 
       //collision of ball with paddles
-      if(bx >= 20 && bx <= 40 && by >= y[0] - 50 && by <= y[0] + 50) {
-        bxc*=-1; 
-      } else if(bx >= 510 && bx <= 530 && by >= y[1] - 50 && by <= y[1] + 50) {
-        bxc*=-1;
+      if(bx >= 40 && bx <= 70 && by >= playerList.get(0).getY() - 50 && by <= playerList.get(0).getY() + 50) {
+        bxc = ballSpeed; 
+      } else if(bx >= 730 && bx <= 780 && by >= playerList.get(1).getY() - 50 && by <= playerList.get(1).getY() + 50) {
+        bxc = ballSpeed * -1;
       } 
+
+      //ball passes edge (WIN)
+      if(bx < 0 || bx > 800) {
+        
+      }
 
       //move ball 
       bx+=bxc;
@@ -82,6 +73,24 @@ public class Pong extends Minigame {
       dc.pause(20);
     }
   
+  }
+
+  @Override
+  public void moveCharacters() {
+    boolean[] movementAllowance = {true, true, true, true};
+
+    for(int i = 0; i < this.playerList.size(); i++) {
+      if(this.playerList.get(i) != null) {
+        boolean[] tempControl = this.playerList.get(i).getControl().getPlayerKeysPressed(); // gets the player's currently pressed keys
+  
+        if(tempControl[0] && movementAllowance[0]) { //if that player's up key is pressed (w for player 1, t for player 2, etc.)
+          playerList.get(i).moveY(-5);
+        }
+        if(tempControl[2] && movementAllowance[2]) { //down
+          playerList.get(i).moveY(5);
+        }
+      }
+    }
   }
   
 }
